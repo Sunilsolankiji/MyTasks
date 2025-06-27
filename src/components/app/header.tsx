39072d047bf6ba8a76c96ba92ee5e6c3fbab5597
Sunil Solanki <sunilsolanki630s@gmail.com>
2025-06-27 15:33:29 +0000

    Make the following changes:
    On the page src/components/app/task-page.ts

--- src/components/app/header.tsx
+++ src/components/app/header.tsx
@@ -11,14 +11,12 @@ interface HeaderProps {
 export function Header({ onOpenTaskDialog }: HeaderProps) {
   return (
     <header className="sticky top-0 z-10 w-full border-b border-gray-200 backdrop-blur-md bg-white/30 p-4 dark:border-gray-700 dark:bg-gray-800/30">
-      <div className="container flex h-14 items-center justify-between">
-        <div className="mr-4 hidden md:flex">
-          <a className="mr-6 flex items-center space-x-2" href="/">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" className="h-6 w-6"><rect width="256" height="256" fill="none"></rect><path d="M128,24a104,104,0,1,0,104,104A104.2,104.2,0,0,0,128,24Zm0,192a88,88,0,1,1,88-88A88.1,88.1,0,0,1,128,216Zm48-88a48,48,0,0,1-96,0Z" fill="currentColor"></path></svg>
-            <span className="hidden font-bold sm:inline-block">MyTasks</span>
-          </a>
-        </div>
-        <div className="flex flex-1 items-center justify-end space-x-2">
+      <div className="container flex h-14 items-center justify-center gap-6">
+        <a className="flex items-center space-x-2" href="/">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" className="h-6 w-6"><rect width="256" height="256" fill="none"></rect><path d="M128,24a104,104,0,1,0,104,104A104.2,104.2,0,0,0,128,24Zm0,192a88,88,0,1,1,88-88A88.1,88.1,0,0,1,128,216Zm48-88a48,48,0,0,1-96,0Z" fill="currentColor"></path></svg>
+          <span className="font-bold sm:inline-block">MyTasks</span>
+        </a>
+        <div className="flex items-center space-x-2">
           <Button onClick={onOpenTaskDialog}>
             <Plus className="mr-2 h-4 w-4" />
             Add Task
